340c83a96320c4b135819586dd24d6eba1fddf57
nagasai9154 <104343780+nagasai9154@users.noreply.github.com>
2024-01-24 08:59:59 +0530

    Update provider.tf

--- Hotstar-Clone-main/EKS_TERRAFORM/provider.tf
+++ Hotstar-Clone-main/EKS_TERRAFORM/provider.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  access_key = "AKIAW3C3EKIXBBFBIL2V"
-  secret_key = "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"
-  region = "us-east-1"
-}
-
 
 terraform {
   required_providers {
@@ -16,5 +10,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-south-1"
+  region = "us-east-1"
 }
